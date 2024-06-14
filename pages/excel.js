c815ae3f91f3ca36fb72c9316e8a7182e06ec5bd
Shoffly <80728185+Shoffly@users.noreply.github.com>
2024-06-14 10:05:22 +0000

    Added something to excel

---
+++ pages/excel.js
@@ -0,0 +1,13 @@
+// pages/excel.js or pages/excel.jsx
+
+import React from 'react';
+
+const ExcelPage = () => {
+  return (
+    <div>
+      <h1>Excel Page</h1>
+    </div>
+  );
+};
+
+export default ExcelPage;

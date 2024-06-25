d4c25d91bfe5c5b9a7662aa0886ee4685754a765
Shoffly <80728185+Shoffly@users.noreply.github.com>
2024-06-25 09:40:51 +0000

    Removed the : that I didn't like.

--- pages/j-deets.js
+++ pages/j-deets.js
@@ -56,14 +56,14 @@ export default function JourneyDetails() {
       <h1 className={styles.title}>Journey Details</h1>
       {journeyDetails && (
         <div className={styles.journeyDetails}>
-          <h4 className={styles.description}>Name:<br></br> {journeyDetails.name}</h4>
-          <p className={styles.description}>Description:<br></br> {journeyDetails.description}</p>
+          <h4 className={styles.description}>Name<br></br> {journeyDetails.name}</h4>
+          <p className={styles.description}>Description<br></br> {journeyDetails.description}</p>
           <h3 className={styles.subtitle}>Steps</h3>
           {steps.map((step, index) => (
             <div key={index} className={styles.step}>
               <h3 className={styles.stepTitle}>Step {index + 1}</h3>
               <p className={styles.stepUsers}><strong>Users on this step:</strong> {usersPerStep[step.step_number] || 0}</p> <div className={styles.deets}> 
-              <p className={styles.stepDetail}><strong>Trigger Type:</strong> {step.trigger_type}</p>
+              <p className={styles.stepDetail}><strong>Trigger Type</strong> {step.trigger_type}</p>
               <p className={styles.stepDetail}><strong>Trigger Value:</strong> {step.trigger_value}</p>
               <p className={styles.stepDetail}><strong>Action Type:</strong> {step.action_type}</p>
               <p className={styles.stepDetail}><strong>Delay Interval:</strong> {step.delay_interval} days</p>
